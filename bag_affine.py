3aa5593e0b15f6bc0c948a21f733a20601846fad
Nikita Korneich <korneich.nik@gmail.com>
2023-04-27 12:53:11 +0300

    affine transf

--- bag_affine.py
+++ bag_affine.py
@@ -43,9 +43,8 @@ def main():
 
         trans = np.loadtxt("matrix.txt")
         R, t = trans[:, [0, 1, 2]], trans[:, [3]].reshape(3)
-        print(t)
         t[[0, 1]] = t[[1, 0]]
-        print(t)
+
         align_to = rs.stream.depth
         align = rs.align(align_to)
         fltr = rs.hole_filling_filter(2)
@@ -53,6 +52,7 @@ def main():
         profile = pipeline.start(config)
 
         while True:
+            # workspace coord
             # 100 30
             # 950 330
             for x in range(5):
@@ -71,7 +71,7 @@ def main():
             #depth = cv.inpaint(depth, mask, 3, flags=cv.INPAINT_NS)
             color_image = np.asanyarray(color_frame.get_data())
             # take only points in workspace
-            color_image_crop = color_image[30:330, 100:950, :]
+            # color_image_crop = color_image[30:330, 100:950, :]
             pc = rs.pointcloud()
             pc.map_to(color_frame)
             depth_frame_fltr = fltr.process(depth_frame)
@@ -80,11 +80,10 @@ def main():
             
             depth_intrinsics = rs.video_stream_profile(depth_frame.profile).get_intrinsics()
             w, h = depth_intrinsics.width, depth_intrinsics.height
-            verts = np.asarray(points_pc.get_vertices(2)).view(np.float32).reshape(h, w, 3)*[1000, 1000, 1000]
+            verts = np.asarray(points_pc.get_vertices(2)).view(np.float32).reshape(h, w, 3)*1000
             # take only points in workspace
-            print(verts.shape)
-            verts = verts[30:330, 100:950, :]
-            w_dpth, h_dpth = verts.shape[1], verts.shape[0]
+            #verts = verts[30:330, 100:950, :]
+
             verts = verts.reshape(-1, 3)
 
             # affine transform camera point to robot system
@@ -94,7 +93,7 @@ def main():
 
             # uv - texture coordinates of points in the pointcloud (0..1, 0..1) normilized by width and height of the color image
             tex = np.asanyarray(points_pc.get_texture_coordinates(2)).reshape(h, w, 2)
-            tex = tex[30:330, 100:950, :]
+            #tex = tex[30:330, 100:950, :]
             tex = tex.reshape(-1, 2)
             h = color_image.shape[0]
             w = color_image.shape[1]
@@ -110,10 +109,10 @@ def main():
             rbt_pnts_align = rbt[ind]#.astype(np.int16)
             tex_align = tex[ind]
 
-            h = color_image_crop.shape[0]
-            w = color_image_crop.shape[1]
+            h = color_image.shape[0]
+            w = color_image.shape[1]
             cond = (rbt_pnts_align[:, 0] > 0) & (rbt_pnts_align[:, 0] < w) & \
-                (rbt_pnts_align[:, 1] > 0) & (rbt_pnts_align[:, 1] < h) & (rbt_pnts_align[:, 2] != 0)
+                (rbt_pnts_align[:, 1] > 0) & (rbt_pnts_align[:, 1] < h) # & (rbt_pnts_align[:, 2] != 0)
             
             rbt_pnts_align = rbt_pnts_align[cond]
 
@@ -123,8 +122,8 @@ def main():
                     torch.from_numpy(rbt_pnts_align[:, 1]).to(torch.int64), \
                     torch.from_numpy(q[cond]).to(torch.int64)
 
-            newimg = torch.zeros((h_dpth, w_dpth, 3), dtype=torch.int64)
-            newimg.index_put_((yt, xt), pt)#, accumulate=True)
+            newimg = torch.zeros((720, 1280, 3), dtype=torch.int64)
+            newimg.index_put_((yt, xt), pt)
 
             transform = T.GaussianBlur(kernel_size=(5, 5), sigma=(0.5, 0.5))
             blurred_img = transform(newimg)
@@ -133,7 +132,7 @@ def main():
             plt.subplot(121)
             plt.imshow(newimg)
             plt.subplot(122)
-            plt.imshow(color_image_crop)
+            plt.imshow(color_image)
             plt.show()
             break
             
